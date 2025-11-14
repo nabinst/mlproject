576dbea4bf4d154e748b68f45866aef3d8f9a443
Nabin Shrestha <nabinst@hotmail.com>
2025-11-14 17:05:29 -0500

    prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import dill
 from src.exception import CustomException 
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+import pickle
 
 
 
@@ -42,5 +43,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
 
         return report 
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
